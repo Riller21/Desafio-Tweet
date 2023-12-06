b8894356dd13707f4ebce35b0f73a66fbada49ae
Riller Rodrigues <rillernonato@yahoo.com.br>
2023-12-06 09:51:01 -0300

    primeiro commit

---
+++ desafio_Tweet.py
@@ -0,0 +1,9 @@
+T = input()
+  
+    
+#Sera verificado se os dados inseridos em T sao menores ou igual 140
+if len(T) <= 140:#usada a funcao len para contabilizar os caracteres de T
+    print("TWEET")
+
+else:
+    print("MUTE")
